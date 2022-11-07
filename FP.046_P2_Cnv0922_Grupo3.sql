0b8c82d34b8a685d5af1ef789ea7ff1491f1c9e1
Daniel Boj <dboj@uoc.edu>
2022-11-07 12:26:58 +0100

    Commit inicial: Creación del script sql

---
+++ FP.046_P2_Cnv0922_Grupo3.sql
@@ -0,0 +1,23 @@
+-- PLANTILLA DE ENTREGA DE LA PARTE PRÁCTICA DE LAS ACTIVIDADES
+-- --------------------------------------------------------------
+-- Actividad: FP.046_PRODUCTO2
+--
+-- Grupo: Cnv0922_Grupo03: Nínive
+-- 
+-- Integrantes: 
+-- 1. Katiane Coutinho Rosa
+-- 2. Miki Alvarez Vidal
+-- 3. Daniel Zafra del Pino
+-- 4. Daniel Boj Cobos
+--
+-- Database: ICX0_P3_3
+-- --------------------------------------------------------------
+
+USE ICX0_P3_3;
+
+-- Consultas Externas
+
+--
+-- Pregunta 1
+--
+
